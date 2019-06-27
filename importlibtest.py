2837baca29fdbd96a62ff41a18e542c254c8ef8d
Shintarou Okada <kokuzen@gmail.com>
2019-06-27 13:18:47 +0900

    Update importlibtest.py

--- importlibtest.py
+++ importlibtest.py
@@ -1,6 +1,8 @@
 import importlib
 import fire
 
+# git submoduleからimportするにはPYTHONPATHの設定が必要
+# python submoduleも普通にfoo.barでいける
 def im(module='detnas', model='DetNASSmallCOCO', kwargs='{}'):
     m = importlib.import_module(module)
     #m.DetNASSmallCOCO(**eval(kwargs))
